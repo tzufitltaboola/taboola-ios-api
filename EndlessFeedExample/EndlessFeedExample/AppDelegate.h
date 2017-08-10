3dc4256457bf26833113fab535c07c9878054c72
Roman Slyepko <roma.srt@gmail.com>
2017-08-10 18:10:36 +0300

    added EndlessFeedExample

---
+++ EndlessFeedExample/EndlessFeedExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EndlessFeedExample
+//
+//  Copyright © 2017 Taboola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
